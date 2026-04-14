c660c0c3bc8c1958fd16a453581a1aff44f6570d
Anatolii Honchar <gonchar_tol@ukr.net>
2026-04-14 13:38:30 +0300

    Add metadata for 404 page with Open Graph support

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,30 @@
 import css from "./Home.module.css";
 import Link from "next/link";
+import type { Metadata } from "next";
+
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
+export const metadata: Metadata = {
+  metadataBase: new URL(baseUrl as string),
+
+  title: "404 - Page not found",
+  description:
+    "This page does not exist. The requested page could not be found.",
+
+  openGraph: {
+    title: "404 - Page not found",
+    description:
+      "This page does not exist. The requested page could not be found.",
+    images: [
+      {
+        url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Notehub - Page not found",
+      },
+    ],
+  },
+};
 
 export default function NotFound() {
   return (
